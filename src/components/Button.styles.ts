c6c6e34cbc96c60a91e6540822a1b7e82904018f
irailtonreis <irailton.oliveira91@gmail.com>
2023-08-04 19:01:32 -0300

    style: lint da aplicação - configuração eslint

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -2,12 +2,12 @@ import styled from 'styled-components'
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success'
 
-interface ButtonContainerProps   {
-    variant: ButtonVariant
+interface ButtonContainerProps {
+  variant: ButtonVariant
 }
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
-    width: 100px;
-    height: 40px;
-    background-color: ${props => props.theme['green-500']};
+  width: 100px;
+  height: 40px;
+  background-color: ${(props) => props.theme['green-500']};
 `

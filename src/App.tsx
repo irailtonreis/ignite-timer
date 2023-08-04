0dca5ec2771e79c24da702f08c5785f29da04fad
irailtonreis <irailton.oliveira91@gmail.com>
2023-08-04 18:11:13 -0300

    feat: Estrutura da aplicação - Tema default

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
 
 
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
      <Button />
      <Button />
      <Button />
      <Button />
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -9,6 +9,6 @@ interface ButtonContainerProps   {
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
-    
+    background-color: ${props => props.theme.primary};
 
 `
